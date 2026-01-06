62297c62bbbdd524482f5d4618ed68f3ace39ebf
Vynicius <vynicius.henrique15@gmail.com>
2026-01-06 00:41:42 -0300

    commit

--- src/test/java/com/vynicius/Locadora/service/CarroServiceTest.java
+++ src/test/java/com/vynicius/Locadora/service/CarroServiceTest.java
@@ -1,7 +1,9 @@
 package com.vynicius.Locadora.service;
 
 import com.vynicius.Locadora.entity.CarroEntity;
+import com.vynicius.Locadora.exeptions.CarroNaoEncontradoException;
 import com.vynicius.Locadora.repository.CarroRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -27,10 +29,61 @@ class CarroServiceTest {
     @Test
     @DisplayName("Deve salvar um carro")
     void salvarCarro(){
-        Mockito
-                .when(repository.findById(1L))
-                .thenReturn(Optional.of(new CarroEntity("Sedan", BigDecimal.valueOf(270.00), 2020)));
+        CarroEntity carro = new CarroEntity("Civic", BigDecimal.valueOf(200.00), 2025);
 
-        System.out.println(repository.findById(1L));
+        Mockito.when( repository.save(Mockito.any()) ).thenReturn(carro);
+
+        var carroSalvo = service.salvar(carro);
+
+        assertNotNull(carroSalvo);
+        Mockito.verify(repository).save(Mockito.any());
+    }
+
+    @Test
+    @DisplayName("Deve apontar um erro ao tentar salvar um carro com valor de diária negativa")
+    void erroSalvar(){
+
+        // 1 - Cenário
+        CarroEntity carro = new CarroEntity("Civic", BigDecimal.valueOf(-200.00), 2025);
+
+        // 2 - Execução
+        Throwable throwable = Assertions.catchThrowable(() -> service.salvar(carro));
+
+        // 3 - Verificação
+        Assertions.assertThat(throwable).isInstanceOf(IllegalArgumentException.class);
+
+        Mockito.verify(repository, Mockito.never()).save(Mockito.any());
+    }
+
+    @DisplayName("Deve atualizar um carro")
+    @Test
+    void atualizarCarro(){
+        CarroEntity carroExistente = new CarroEntity("Civic", BigDecimal.valueOf(200.00), 2025);
+        CarroEntity carroAtualizado = new CarroEntity("Sedan", BigDecimal.valueOf(300.00), 2015);
+        carroAtualizado.setId(1L);
+
+        Mockito.when( repository.findById(1L)).thenReturn(Optional.of(carroExistente));
+
+        Mockito.when( repository.save(Mockito.any()) ).thenReturn(carroAtualizado);
+
+        CarroEntity resultado = service.atualizar(1L, carroAtualizado);
+
+        assertEquals("Sedan", resultado.getModelo());
+
+        Mockito.verify(repository, Mockito.times(1)).save(Mockito.any());
+    }
+
+    @Test
+    @DisplayName("Deve apontar para um erro quando tentar atualizar um carro inexistente")
+    void erroAtualizar(){
+        CarroEntity carroExistente = new CarroEntity("Civic", BigDecimal.valueOf(200.00), 2025);
+
+        Mockito.when(repository.findById(Mockito.any())).thenReturn(Optional.empty());
+
+        var error = Assertions.catchThrowable( () -> service.atualizar(1L, carroExistente));
+
+        Assertions.assertThat(error).isInstanceOf(CarroNaoEncontradoException.class);
+
+        Mockito.verify(repository, Mockito.times(0)).save(Mockito.any());
     }
 }
